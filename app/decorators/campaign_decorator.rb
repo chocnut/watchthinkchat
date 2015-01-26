6fa9e805e20e2e0fb466af5784fdb90ff715856e
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-01-27 11:04:06 +1300

    start on stats

--- app/decorators/campaign_decorator.rb
+++ app/decorators/campaign_decorator.rb
@@ -9,4 +9,32 @@ class CampaignDecorator < Draper::Decorator
     return "http://#{url}.#{ENV['base_url']}" if subdomain?
     "http://#{url}"
   end
+
+  def unique_visitors
+    1000
+  end
+
+  def starting_video
+    86
+  end
+
+  def starting_video_percentage
+    ((starting_video.to_d / unique_visitors.to_d) * 100).to_i
+  end
+
+  def finishing_video
+    90
+  end
+
+  def finishing_video_percentage
+    ((finishing_video.to_d / unique_visitors.to_d) * 100).to_i
+  end
+
+  def signing_up
+    500
+  end
+
+  def signing_up_percentage
+    ((signing_up.to_d / unique_visitors.to_d) * 100).to_i
+  end
 end
