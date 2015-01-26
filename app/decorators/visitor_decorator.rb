3f83deacb6b144284fe1bad5d0d14fde7f5e4dff
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-01-27 12:20:38 +1300

    subscription

--- app/decorators/visitor_decorator.rb
+++ app/decorators/visitor_decorator.rb
@@ -11,17 +11,33 @@ class VisitorDecorator < Draper::Decorator
 
   def content(campaign)
     return nil unless campaign.growthspace.try(:enabled?)
-    access_id_and_access_secret(campaign.growthspace)
+    access_id_and_access_secret_content(campaign.growthspace)
     Campaign::Growthspace::Content.find(params: { route_id: latest_route_id(campaign),
                                                   language_id: I18n.locale })
   end
 
+  def subscribe(campaign)
+    return nil unless campaign.growthspace.try(:enabled?)
+    access_id_and_access_secret_subscribers(campaign.growthspace)
+    Campaign::Growthspace::Subscriber.create(first_name: first_name,
+                                             last_name: last_name,
+                                             email: email,
+                                             route_id: latest_route_id(campaign),
+                                             language_code: I18n.locale)
+  end
+
   private
 
-  def access_id_and_access_secret(growthspace)
+  def access_id_and_access_secret_content(growthspace)
     return if growthspace.api_key.blank? && growthspace.api_secret.blank?
     Campaign::Growthspace::Content.access_id = growthspace.api_key
     Campaign::Growthspace::Content.access_secret = growthspace.api_secret
+  end
+
+  def access_id_and_access_secret_subscribers(growthspace)
+    return if growthspace.api_key.blank? && growthspace.api_secret.blank?
+    Campaign::Growthspace::Subscriber.access_id = growthspace.api_key
+    Campaign::Growthspace::Subscriber.access_secret = growthspace.api_secret
   end
 
   def latest_route_id(campaign)
