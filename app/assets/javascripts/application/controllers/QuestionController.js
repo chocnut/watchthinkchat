4298d64d7583ac41b36c98ac667ebd6031ff824f
adammeyer <adam.meyer@cru.org>
2015-02-19 10:44:09 -0500

    Wait to run jumpTo function until interaction is sent to the api

--- app/assets/javascripts/application/controllers/QuestionController.js
+++ app/assets/javascripts/application/controllers/QuestionController.js
@@ -1,14 +1,7 @@
-angular.module('chatApp').controller('QuestionController', function ($scope, jumpTo, question, nextQuestion, api) {
+angular.module('chatApp').controller('QuestionController', function ($scope, jumpTo, question, nextQuestion) {
   $scope.question = question;
 
   $scope.buttonClick = function(option){
-    api.interaction({
-      interaction: {
-        resource_id: option.id,
-        resource_type: option.resource_type,
-        action: 'click'
-      }
-    });
     jumpTo.jump(option, nextQuestion);
   };
 });
--- app/assets/javascripts/application/services/jumpTo.js
+++ app/assets/javascripts/application/services/jumpTo.js
@@ -1,20 +1,28 @@
 angular.module('chatApp')
-    .service('jumpTo', function ($rootScope, $location) {
+    .service('jumpTo', function ($rootScope, $location, api) {
       this.jump = function (option, nextQuestion) {
-        switch (option.conditional) {
-          case 'next':
-            if(angular.isDefined(nextQuestion)){
-              $location.path('/q/' + nextQuestion.code);
-            }else{
+        api.interaction({
+          interaction: {
+            resource_id: option.id,
+            resource_type: option.resource_type,
+            action: 'click'
+          }
+        }).then(function(){
+          switch (option.conditional) {
+            case 'next':
+              if(angular.isDefined(nextQuestion)){
+                $location.path('/q/' + nextQuestion.code);
+              }else{
+                $location.path('/final');
+              }
+              break;
+            case 'skip':
+              $location.path('/q/' + option.conditional_question_id);
+              break;
+            case 'finish':
               $location.path('/final');
-            }
-            break;
-          case 'skip':
-            $location.path('/q/' + option.conditional_question_id);
-            break;
-          case 'finish':
-            $location.path('/final');
-            break;
-        }
+              break;
+          }
+        });
       };
     });
