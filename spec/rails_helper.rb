822979ac1881a52451cb7e3e1c792e2286384407
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-02-25 12:05:01 +1300

    restore codeclimate

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,19 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
 require 'common_helper'
 
 require 'capybara/rspec'
