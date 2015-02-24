54f4161a1268216fcb3e60bec3092565bc4bf630
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-02-24 21:08:57 +1300

    repo tidy and remove code climate for now

--- spec/common_helper.rb
+++ spec/common_helper.rb
@@ -1,4 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
+# if ENV['CODECLIMATE_REPO_TOKEN']
+#   require 'codeclimate-test-reporter'
+#   CodeClimate::TestReporter.start
+# else
+#   require 'simplecov'
+#   SimpleCov.start 'rails'
+# end
+
 require 'shoulda/matchers'
 require 'rspec/rails'
 
