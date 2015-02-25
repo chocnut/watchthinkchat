e8b6ba8d4ff3c6bbfd56ade2fceb2bcc1befd2bb
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-02-26 11:16:21 +1300

    add rails loading back in

--- spec/common_helper.rb
+++ spec/common_helper.rb
@@ -1,12 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
-# if ENV['CODECLIMATE_REPO_TOKEN']
-#   require 'codeclimate-test-reporter'
-#   CodeClimate::TestReporter.start
-# else
-#   require 'simplecov'
-#   SimpleCov.start 'rails'
-# end
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
 
+require File.expand_path('../../config/environment', __FILE__)
 require 'shoulda/matchers'
 require 'rspec/rails'
 
