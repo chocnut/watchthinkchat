842f7114bbb348e7ffd8e3e5d789b7ec894683c2
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-03-25 14:19:55 +1300

    rubocop fixes

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: ENV['AWS_SES_ACCESS_ID'],
-  secret_access_key: ENV['AWS_SES_ACCESS_SECRET']
+                                       access_key_id: ENV['AWS_SES_ACCESS_ID'],
+                                       secret_access_key: ENV['AWS_SES_ACCESS_SECRET']
